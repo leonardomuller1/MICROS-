fbc8a7866cf1136493acf137f712554add10e32a
Leonardo Muller <81366887+leonardomuller1@users.noreply.github.com>
2023-03-16 20:51:59 -0300

    Update exe5.c

--- bit a bit/exe5.c
+++ bit a bit/exe5.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 /*
-Solicite um valor para a variável x e um índice b que indica um dos bits da variável. Ligue
-o bit correspondente. Exiba o valor original e o resultado.
+Solicite um valor para a variável x e um índice b que indica um dos bits da variável.
+Inverta o valor do bit correspondente. Exiba o valor original e o resultado.
 */
 
 int main()
