86bd74838795001fe7bd888eda5b5a4d20fc635b
Leonardo Muller <81366887+leonardomuller1@users.noreply.github.com>
2023-03-17 15:05:37 -0300

    Update exe3.c

--- bit a bit/exe3.c
+++ bit a bit/exe3.c
@@ -6,4 +6,14 @@ Solicite um valor para a variável x e um índice b que indica um dos bits da va
 o bit correspondente. Exiba o valor original e o resultado.
 */
 
+int main()
+{
+    uint8_t posicao = 4; //posicao do bit (equivale ao 32)
+    posicao = posicao ^ 1; // soma o valor
+    uint8_t valor = 88; // valor original
+    printf("Original: %d\n", valor);
+    valor |= 1 << posicao; //ligando o bit especifico
+    printf("Resultado: %d\n", valor);
+
+}
 
