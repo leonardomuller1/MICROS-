d45b601ca62104b15b200aa3a0193cbd93533c0a
Leonardo Muller <81366887+leonardomuller1@users.noreply.github.com>
2023-03-16 21:55:43 -0300

    Update exe3.c

--- bit a bit/exe3.c
+++ bit a bit/exe3.c
@@ -6,16 +6,4 @@ Solicite um valor para a variável x e um índice b que indica um dos bits da va
 o bit correspondente. Exiba o valor original e o resultado.
 */
 
-int main()
-{
-    uint8_t x = 1, b=3,x2,x3;
-    int i;
-    x2=1;
-    for (i=0;i<b;i++){
-        x2 = x2<<1;
-    }
-    x3 = x|x2;
-    printf("%d\n", x);
-    printf("%d\n", x3);
-    return 0;
-}
+
